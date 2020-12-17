626ebc5808a2f7c44084fcc3b6863927e3fbf2a5
LUCASSURFACE\Lucas Schweickert <lucassc@kth.se>
2020-12-17 17:16:29 +0100

    improved demo script

--- src/pfeifferTC110/demo_pfeifferTC110.py
+++ src/pfeifferTC110/demo_pfeifferTC110.py
@@ -1,14 +1,12 @@
 #%%
-# For testing the pump functions
 import PfeifferTC110
-pump = PfeifferTC110.TC110()
-
-#%%
-# For testing what the pump identifies as
-import visa
-rm = visa.ResourceManager('@py')
-devices = rm.list_resources()
-print(devices)
+try:
+    pump = PfeifferTC110.TC110(autoconnect=False)
+    # For testing which port the pump is connected to
+    devices = pump.rm.list_resources()
+    print(devices)
+finally:
+    pump.close()
 
 # %%
 # Demonstrate pump
