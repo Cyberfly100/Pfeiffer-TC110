57ba71782ad10446305cec473b6f4f27e521eaeb
LUCASSURFACE\Lucas Schweickert <lucassc@kth.se>
2020-12-18 12:08:37 +0100

    fixed requirements

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'pyvisa-py'
-        'pyusb'
+        'pyvisa-py',
+        'pyusb',
         'pyserial'
     ]
 )
